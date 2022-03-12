25d80e7cf75365b41e46a9130cce4b71a8a89a53
Carlos Wei <carlos.wei@armchina.com>
2022-03-11 20:54:46 -0800

    [elf-lab-01] add elf lab 01: strong/weak symbol
    Signed-off-by: Carlos Wei <carlos.wei@armchina.com>

--- lab/src/kernel.c
+++ lab/src/kernel.c
@@ -1,20 +1,24 @@
 #include "uart.h"
 
-extern void ldr_test(void);
-
-void my_ldr_str_test(void)
-{
-
-	ldr_test();
-}
+// weak symbol
+__attribute__((weak)) int symbol = 1;
+__attribute__((weak)) int extern_func(int a, int b);
 
 void kernel_main(void)
 {
-	uart_init();
-	uart_send_string("Welcome BenOS!\r\n");
+    static int var_1 = 85;
+    static int var_2;
+    int c = 6;
+    int d;
 
-	/* my test*/
-	my_ldr_str_test();
+	uart_init();
+	uart_send_string("test weak symbol!\r\n");
+    if (extern_func) {
+        extern_func(c, d);
+        uart_send_string("extern.c: extern_func impl\n");
+    } else {
+        uart_send_string("no extern_func impl\n");
+    }
 
 	while (1) {
 		uart_send(uart_recv());
