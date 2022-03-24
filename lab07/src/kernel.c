ad9d138e6c9016d399a5e88ef24a639de3bc87df
Carlos Wei <carlos.wei@armchina.com>
2022-03-24 01:26:42 -0700

    [lab07] ldxr, stxr ins
    1. add ldxr, stxr exmaples
    2. test_my_atomic_write

--- lab07/src/kernel.c
+++ lab07/src/kernel.c
@@ -1,15 +1,22 @@
 #include "uart.h"
 
+extern int my_test_data;
 extern void test_adr(void);
+extern int my_atomic_write(int x);
 
 void kernel_main(void)
 {
-	unsigned long ret = 0;
+	int ret = 0;
+
 	uart_init();
 	uart_send_string("start test!\r\n");
-	test_adr();
+	ret = my_test_data;
+	my_atomic_write(0x34);
+	ret = my_test_data;
+	if (ret == 0x34) {
+		uart_send_string("pass test!\r\n");
+	}
 	uart_send_string("end test!\r\n");
-
 	while (1) {
 		uart_send(uart_recv());
 	}
