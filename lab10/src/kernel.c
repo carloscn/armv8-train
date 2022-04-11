42fd42a924e6b76d9bd3dd8a184352f760c12be5
Carlos Wei <carlos.wei.hk@gmail.com>
2022-04-11 15:43:33 +0800

    [armv8] inline assembly
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

--- lab10/src/kernel.c
+++ lab10/src/kernel.c
@@ -1,21 +1,14 @@
 #include "uart.h"
 
-extern void ldr_test(void);
-
-void my_ldr_str_test(void)
-{
-
-	ldr_test();
-}
+extern void test_memcpy(void);
+extern void test_memset(void);
 
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
-
 	/* my test*/
-	my_ldr_str_test();
-
+	test_memcpy();
 	while (1) {
 		uart_send(uart_recv());
 	}
