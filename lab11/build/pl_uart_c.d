127b49f5f424131e59a244f4f0816280240a05f6
Carlos Wei <carlos.wei.hk@gmail.com>
2022-04-14 14:08:12 +0800

    [lab11] switch el2 to el1
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

---
+++ lab11/build/pl_uart_c.d
@@ -0,0 +1,2 @@
+build/pl_uart_c.o: src/pl_uart.c include/asm/pl_uart.h include/asm/base.h \
+ include/asm/gpio.h include/asm/base.h include/io.h
--- lab11/src/kernel.c
+++ lab11/src/kernel.c
@@ -1,20 +1,9 @@
 #include "uart.h"
 
-extern void ldr_test(void);
-
-void my_ldr_str_test(void)
-{
-
-	ldr_test();
-}
-
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
-
-	/* my test*/
-	my_ldr_str_test();
 
 	while (1) {
 		uart_send(uart_recv());
